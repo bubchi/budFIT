9f8be6cfd9b6bb793abb1f5f552648f0aace734c
bubchi <petra.lassakova@gmail.com>
2022-06-15 18:49:38 +0200

    shared preferences

--- app/src/main/java/com/example/budfit/MainActivity.kt
+++ app/src/main/java/com/example/budfit/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.budfit
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,28 +8,47 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
-import androidx.fragment.app.DialogFragment
 import com.example.budfit.databinding.ActivityMainBinding
-import com.google.android.material.textfield.TextInputLayout
-import android.widget.EditText as Text
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private var progres = 0
+    private var pocitadlo = 0
 
+    private lateinit var voda: String
+    private var kalorie: Int = 0
+
+    private lateinit var vodaText : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val button: Button = binding.button
-        button.setOnClickListener(View.OnClickListener {
+        loadData()
+        updateProgres()
+
+
+        //inicializacia btnu na otvorenie BMI aktivity,
+        val buttonOpenBMI: Button = binding.button
+        buttonOpenBMI.setOnClickListener(View.OnClickListener {
             openBMIActivity();
         })
-        val vodaText : TextView = binding.vodaText
+
+
+        val kcalJedlo: TextView = binding.kcalJedlo
+        kcalJedlo.text = kalorie.toString()
+
+        //vodaText = binding.vodaText
+        val vodaText = binding.vodaText
         val vodaButton: ImageButton = binding.vodaButton
-        var pocitadlo = 0
+
+
+        //vodaText.text = pocitadlo.toString()
+        //vodaText.text = voda
+
+        if (pocitadlo > 0)
+            vodaText.text = pocitadlo.toString()
 
 
         vodaButton.setOnClickListener() {
@@ -38,7 +58,9 @@ class MainActivity : AppCompatActivity() {
                 progres += 10
             updateProgres()
 
+            saveData()
         }
+        saveData()
         binding.vyberJedlaButton.setOnClickListener() {
             openVyberJedlaActivity()
         }
@@ -75,55 +97,32 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    /*//var vyska = inputVyska.edit.
-    var vek = findViewById(R.id.inputVek) as TextInputLayout
-    var vaha = findViewById(R.id.inputVaha) as TextInputLayout
-    //var textVysledok = findViewById(R.id.textViewResult) as TextView
-    val buttonVypocitaj = findViewById(R.id.button) as Button
+    private  fun saveData() {
+        val sharedPreferences = getSharedPreferences("SharedP", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.apply(){
+            putInt("check", pocitadlo)
+            putInt("p", progres)
+            putInt("kcal", kalorie)
+            //putString("voda", voda)
+        }.apply()
+    }
+    private  fun loadData() {
+        val sharedPreferences =getSharedPreferences("SharedP", Context.MODE_PRIVATE)
+        pocitadlo =sharedPreferences.getInt("check", 0)
+        progres = sharedPreferences.getInt("p", 0)
+        kalorie = sharedPreferences.getInt("kcal", 0)
+        //val savedString : String? = sharedPreferences.getString("voda", null)
+        //vodaText.text = pocitadlo.toString()
+    }
 
-    buttonVypocitaj.setOnClickListener{
-        //textVysledok.text = vyska.toString()
 
-
-    }*/
-
-        /*fun pocitajBMI(view: View) {
-            var vyska = findViewById<EditText>(R.id.inputVyska)
-            var msg = vyska.text.toString()
-
-            val textVysledok = findViewById<TextView>(R.id.textViewResult).apply {
-                text = msg
-            }
-
-        }*/
-
-    /*fun pocitajBMI(vyska: Int, vaha: Int): Float {
-
-    }*/
+    private  fun reciveData() {
+        kalorie = intent.getIntExtra("kalorie", 0)
+    }
 
 
 
-    /*fun pocitajBMI(view: android.view.View) {
-        var editVyska = findViewById<android.widget.EditText>(R.id.editTextVyska)
-
-        var editVaha = findViewById<android.widget.EditText>(R.id.editTextTextVaha)
-        var editVek = findViewById<android.widget.EditText>(R.id.editTextVek)
-
-        var vyska = editVyska.text.toString().toInt() / 100
-        var vaha = editVaha.text.toString().toInt()
-        var vek = editVek.text.toString().toInt()
-        var msg = vaha - vyska
-
-        val textVysledok = findViewById<TextView>(R.id.textViewResult).apply {
-             text = msg.toString()
-        }
-    }*/
-
-    /*fun pocitaj(vyska: Int, vaha: Int){
-        var vyska = editVyska
-        var vaha = editVaha
-        var vysledok = vyska. + vaha
-    }*/
 
 
 }
--- app/src/main/java/com/example/budfit/VyberJedlaActivity.kt
+++ app/src/main/java/com/example/budfit/VyberJedlaActivity.kt
@@ -1,16 +1,11 @@
 package com.example.budfit
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
 import android.widget.Toast
-import androidx.appcompat.R
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.budfit.databinding.ActivityVyberJedlaBinding
 
@@ -18,30 +13,41 @@ class VyberJedlaActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityVyberJedlaBinding
     private lateinit var jedla: Jedla
+    private var Kcal: Int = 0
+    private lateinit var mnozstva : ArrayList<String>
+    //private var mnozstvoMasa = binding.mnozstvoMasa.text.toString()
+    //private var mnozstvoPrilohy = binding.mnozstvoPrilohy.text.toString()
+    //private var mnozstvoZeleniny = binding.mnozstvoZeleniny.text.toString()
+    //private var mnozstvoOvocia = binding.mnozstvoOvocia.text.toString()
+    //private var mnozstvoObylnin = binding.mnozstvoObylnin.text.toString()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityVyberJedlaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //val spinner: Spinner = findViewById(R.id.priloha)
-
-        val customList = listOf("1", "2", "3", "4")
-        val c = listOf(jedla)
-        //val adapter = ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, c)
-
-        //binding.ovocie.adapter =adapter
+        /*mnozstva.add(mnozstvoMasa)
+        mnozstva.add(mnozstvoPrilohy)
+        mnozstva.add(mnozstvoZeleniny)
+        mnozstva.add(mnozstvoOvocia)
+        mnozstva.add(mnozstvoObylnin)*/
 
         var ryza: Float = 0F;
 
-        var spinnerPriloha = binding.priloha.adapter.getItem(1)
+
+        /*var spinnerPriloha = binding.priloha.adapter.getItem(1)
         if (spinnerPriloha.equals(0)) {
             ryza = Jedla.RYZA.kalorie
-        }
+        }*/
 
         println("mmmmmmmmmmmmmm")
         binding.priloha.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
                 val objekt = adapterView?.getItemAtPosition(position)
 
 
@@ -54,22 +60,52 @@ class VyberJedlaActivity : AppCompatActivity() {
 
         //val b = binding.maso.get
         binding.button3.setOnClickListener() {
-            Toast.makeText(this@VyberJedlaActivity, " kcal " + ryza.toString() + " " + "", Toast.LENGTH_SHORT).show()
+            if (binding.mnozstvoMasa.text.toString().toFloatOrNull() != null || binding.mnozstvoPrilohy.text.toString().toIntOrNull() != null ||
+                binding.mnozstvoZeleniny.text.toString().toIntOrNull() != null || binding.mnozstvoOvocia.text.toString().toIntOrNull() != null ||
+                binding.mnozstvoObylnin.text.toString().toIntOrNull() != null
+            ) {
+                Jedla.values().forEach {
+                    if (it.meno == binding.maso.selectedItem.toString() || it.meno == binding.priloha.selectedItem.toString() ||
+                        it.meno == binding.zelenina.selectedItem.toString() || it.meno == binding.ovocie.selectedItem.toString() ||
+                        it.meno == binding.obylniny.selectedItem.toString()) {
+                        /*Log.v(
+                            "RYZA menoASDASDAFADFASDASDASD",
+                            binding.priloha.selectedItem.toString()
+                        )*/
+                        Toast.makeText(
+                            this@VyberJedlaActivity,
+                            binding.mnozstvoMasa.text.toString() + " " + it.kalorie.toString(),
+                            Toast.LENGTH_SHORT).show()
+                        Kcal = (binding.mnozstvoMasa.text.toString().toFloat() * it.kalorie.toString().toFloat()).toInt()
+
+
+                        binding.sumaKalorii.text = Kcal.toString() + " " + it.kalorie.toString()
+                        //TODO v cykle prejst formulare a pomocou switchu priradit hodnotu a pripocitat kalorie
+                    }
+                }
+            }
+
+
+
+        }
+
+        binding.backButton.setOnClickListener() {
+            backToMainActivity()
         }
 
 
 
-
-
-
-
     }
 
-    fun main() {
 
-    }
 
-    private fun getKalorie(jedla: Jedla) {
-
+    fun backToMainActivity() {
+        val switchActivityIntent = Intent(this, MainActivity::class.java)
+        if (Kcal != null) {
+            switchActivityIntent.putExtra(
+                "kalorie", Kcal
+            )
+        }
+        startActivity(switchActivityIntent)
     }
 }
